c1b14e5a62402062b70796e6e29997f73a25ae6b
bursachelio <bursak.99@mail.ru>
2023-10-10 16:07:43 +0300

    Добавил ассоциации и изменил методы теста и пользователя с учётом ассоциаий

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,14 @@
 class Test < ApplicationRecord
+  belongs_to :category 
+  belongs_to :author, class_name: "User"
+  
+  has_many :questions 
+  has_many :results 
+  has_many :users, through: :results
+
   def self.sorted_test_names_by_category(category_name)
-    joins("INNER JOIN categories ON categories.id = tests.category_id")
-      .where("categories.title = ?", category_name)
+    joins(:category)
+      .where(categories: { title: category_name })
       .order(title: :desc)
       .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :results
+  has_many :tests, through: :results
+  
   def tests_at_difficulty_level(level)
-    Test.joins('INNER JOIN results ON results.test_id = tests.id')
+    Test.joins(:results)
         .where(results: { user_id: id }, level: level)
   end
 end
