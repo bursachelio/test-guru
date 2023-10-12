f735da4abe5f68c733fa3dcee4e6bc827cc666a5
bursachelio <bursak.99@mail.ru>
2023-10-12 17:05:38 +0300

    добавил для всех has_many - destroy

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category 
   belongs_to :author, class_name: "User"
   
-  has_many :questions 
-  has_many :results 
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
   has_many :users, through: :results
 
   def self.sorted_test_names_by_category(category_name)
