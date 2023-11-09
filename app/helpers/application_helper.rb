6a3e5b63843d45ef0a1f0914ae46a176d7fb58ff
bursachelio <bursak.99@mail.ru>
2023-11-09 17:16:38 +0300

    создал хелперы

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def author_name
+    'Иван Бурсак'
+  end
+
+  def question_header(question)
+    if question.new_record?
+      "Create New #{question.test.title} Question"
+    else
+      "Edit #{question.test.title} Question"
+    end
+  end
+
+  def current_year
+    Time.current.year
+  end
+
+  def github_url(author, repo)
+    link_to 'Проект на GitHub', "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
